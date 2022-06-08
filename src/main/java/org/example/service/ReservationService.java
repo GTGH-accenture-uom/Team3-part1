b5a7bedfe42d46198e98cb004cba44b9dc085eaf
DianaDreou <dreoudiana@gmail.com>
2022-06-08 23:23:05 +0300

    Started a create method.

--- src/main/java/org/example/service/ReservationService.java
+++ src/main/java/org/example/service/ReservationService.java
@@ -1,4 +1,13 @@
 package org.example.service;
 
+import org.example.model.Doctor;
+import org.example.model.Reservation;
+
+import java.util.ArrayList;
+
 public class ReservationService {
+
+    ArrayList<Reservation> reservationList = new ArrayList<>();
+    public void createReservation(){
+    }
 }
