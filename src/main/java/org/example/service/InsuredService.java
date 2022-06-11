0de3abf290ea700b40ea4d8dec13b4151308a3a1
RiaPap <dai16121@uom.edu.gr>
2022-06-11 03:36:03 +0300

    Change the list to map

--- src/main/java/org/example/service/InsuredService.java
+++ src/main/java/org/example/service/InsuredService.java
@@ -1,29 +1,31 @@
 package org.example.service;
 
+import lombok.Data;
 import org.example.model.Insured;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
 This class represents the service for all the insured people.
 It contains a list of all the insured people, and some needed methods
 such as the creation of an insured person.
  */
+@Data
 public class InsuredService {
 
-   List<Insured> insuredList = new ArrayList<Insured>();
+   Map<String,Insured> insuredMap = new HashMap<>();
 
-    //creates new Insured object and adds it to the list
+    //creates new Insured object and adds it to the map
+    //map anti gia list wste na mporw na ta kalw sthn main px sthn createReservation
     public void createInsured(String afm, String amka, String name, String surname, LocalDate birthdate, String email)
     {
-        insuredList.add(new Insured(afm, amka ,name, surname,birthdate, email ));
+        insuredMap.put(amka,new Insured(afm, amka ,name, surname,birthdate, email ));
     }
 
-    //Returns the list with all the insured people.
-    public List<Insured> getAllInsured()
-    {
-        return insuredList;
-    }
+
+
 }
