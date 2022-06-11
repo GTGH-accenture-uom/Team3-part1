851e9e05a36d56ed8933f4a3fb6fd170eee6329e
RiaPap <dai16121@uom.edu.gr>
2022-06-11 03:57:17 +0300

    Create Reservation without boolean delete reservation that provides the insured with the possibility to change reservation

--- src/main/java/org/example/service/ReservationService.java
+++ src/main/java/org/example/service/ReservationService.java
@@ -1,13 +1,33 @@
 package org.example.service;
 
-import org.example.model.Doctor;
-import org.example.model.Reservation;
+import org.example.model.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ReservationService {
 
-    ArrayList<Reservation> reservationList = new ArrayList<>();
-    public void createReservation(){
+
+    //an eixa to idio timeslot se diaforetika centers kai ekana krathsh sto ena
+    // me thn boolean metavlhth m edeixne oti den einai dia8esimo to timeslot oute sto allo center
+
+    private List<Reservation> reservationList = new ArrayList<>();
+    public void createReservation(Insured insured, Timeslot timeslot, VaccinationCenter center){
+        for(Reservation res : reservationList){
+            if(res.getTimeslot().equals(timeslot) && res.getVaccinationCenter().equals(center)){
+                System.out.println("This timeslot is not available.");
+                return;
+            }
+        }
+        reservationList.add(new Reservation(insured, timeslot.getDoctor(), timeslot,center));
+    }
+
+    public void deleteReservation(Insured insured,Timeslot timeslot){
+        reservationList.removeIf(reservation -> reservation.getInsuredPerson().equals(insured));
+        timeslot.setFree(true);
+    }
+    public List<Reservation> getAllRes() {
+        return this.reservationList;
     }
 }
+
